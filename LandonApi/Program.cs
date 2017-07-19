f908c28bc42fb8ebc5edc3686ac805a2f589d767
Yusuf Demir <yusuf@bilgiedin.net>
2017-07-20 00:22:14 +0300

    Restfull api London

--- LandonApi/Program.cs
+++ LandonApi/Program.cs
@@ -17,7 +17,6 @@ namespace LandonApi
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
